ceb91d159ca253f044e9a2b1d4caf2673c924318
Levijan20 <shahanatayev29@gmail.com>
2025-03-25 11:41:43 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,136 @@
+import os
+import random
+from faker import Faker
+import file_operations
+
+SKILLS = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд'
+]
+
+RUNIC_ALPHABET = {
+    'а': 'а͠',
+    'б': 'б̋',
+    'в': 'в͒͠',
+    'г': 'г͒͠',
+    'д': 'д̋',
+    'е': 'е͠',
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
+    'з': 'з̋̋͠',
+    'и': 'и',
+    'й': 'й͒͠',
+    'к': 'к̋̋',
+    'л': 'л̋͠',
+    'м': 'м͒͠',
+    'н': 'н͒',
+    'о': 'о̋',
+    'п': 'п̋͠',
+    'р': 'р̋͠',
+    'с': 'с͒',
+    'т': 'т͒',
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
+    'ц': 'ц̋',
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
+    'ь': 'ь̋',
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
+    'я': 'я̋',
+    ' ': ' ',
+
+    'А': 'А͠',
+    'Б': 'Б̋',
+    'В': 'В͒͠',
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
+    'Е': 'Е',
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
+    'З': 'З̋̋͠',
+    'И': 'И',
+    'Й': 'Й͒͠',
+    'К': 'К̋̋',
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
+    'Н': 'Н͒',
+    'О': 'О̋',
+    'П': 'П̋͠',
+    'Р': 'Р̋͠',
+    'С': 'С͒',
+    'Т': 'Т͒',
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
+    'Я': 'Я̋',
+}
+
+
+
+OUTPUT_FOLDER = 'cards'
+
+
+def generate_runic_skill(skill):
+    return "".join([RUNIC_ALPHABET.get(char, char) for char in skill])
+
+
+def generate_character_card(output_folder, index):
+    fake = Faker('ru_RU')
+
+    name = fake.name()
+    job = fake.job()
+    city = fake.city()
+
+    selected_skills = random.sample(SKILLS, 3)
+
+    runic_skills = [generate_runic_skill(skill) for skill in selected_skills]
+
+    context = {
+        'first_name': name,
+        'last_name': "",
+        'job': job,
+        'town': city,
+        'skill_1': runic_skills[0],
+        'skill_2': runic_skills[1],
+        'skill_3': runic_skills[2],
+        'strength':random.randint(1,30),
+        'agility':random.randint(1,30),
+        'endurance':random.randint(1,30),
+        'intelligence':random.randint(1,30),
+        'luck': random.randint(1,30)
+    }
+
+    output_filename = os.path.join(output_folder, f'result_{index + 1}.svg')
+
+    file_operations.render_template('charsheet.svg', output_filename, context)
+
+
+def main():
+    os.makedirs(OUTPUT_FOLDER, mode=0o777, exist_ok=True)
+
+    for i in range(10):
+        generate_character_card(OUTPUT_FOLDER, i)
+
+
+if __name__ =='__main__':
+    main()
